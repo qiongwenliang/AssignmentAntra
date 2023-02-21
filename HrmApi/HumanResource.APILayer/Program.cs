e6a3aae5bef0127a11beba2bfb0241f95847f3fb
Caterpillar <caterpillar902@gmail.com>
2023-02-21 09:33:37 -0800

    WebAPIApplicationPractice

---
+++ HrmApi/HumanResource.APILayer/Program.cs
@@ -0,0 +1,43 @@
+using Hrm.ApplicationCore.Contract.Repository;
+using Hrm.ApplicationCore.Contract.Service;
+using Hrm.Infrastructure.Data;
+using Hrm.Infrastructure.Repository;
+using Hrm.Infrastructure.Service;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();//add openAPI
+builder.Services.AddControllers(); //it will allow you to work with webAPI
+builder.Services.AddDbContext<HrmDbContext>(options => {
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HrmApiDb"));
+
+});
+
+//Dependency injection
+builder.Services.AddScoped<ICandidateServiceAsync, CandidateServiceAsync>();
+builder.Services.AddScoped<ICandidateRepositoryAsync, CandidateRepositoryAsync>();
+
+
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting(); //middleware, we need to specify so that the route in program.cs will work, allows to use routing
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); }); //map your request to the particular controller
+
+
+app.Run();
+
